7d507d1ed9034de7fd9cc4eb34476ea98a1c3dd4
Logan L <loganhlarlham@gmail.com>
2024-03-22 00:24:21 +0100

    add comments to app.py

--- 04_Dashapp/app.py
+++ 04_Dashapp/app.py
@@ -15,13 +15,16 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from scripts import UserCompareAADR, User_snpfilter
 from scripts.UserCompareAADR import AADR_data, AADR_metadata_subset
 
-
+# initialize the Dash app
 app = Dash(__name__, suppress_callback_exceptions=True)
 app.title = 'Y Haplogroup Matcher'
 
+#get the test files
 userfiles = glob.glob('01_Raw_data/TestUsers/Test*/*_DNA.txt') + glob.glob('01_Raw_data/TestUsers/Test*/*.csv')
 userfiles_options = [{'label': os.path.basename(file).split('_')[0], 'value': file} for file in userfiles]
 
+#Define app layout, dropdown for selecting test file, file upload box for using your own file,
+# haplogroup input, number of results to display, and submit button
 app.layout = html.Div([
     html.Div([
         html.H1('Which ancient dna sample are you most similar to?'),
@@ -82,6 +85,8 @@ app.layout = html.Div([
     html.Div(id='error-message', style={'color': 'red', 'textAlign': 'center'}),
 ])
 
+
+#define callback to parse the uploaded file and display name of file
 @app.callback(
     Output('error-message', 'children'),
     [Input('submit-data', 'n_clicks'),
@@ -105,6 +110,8 @@ def parse_contents(contents, filename):
     decoded = base64.b64decode(content_string)
     return decoded
 
+
+#callback to take info from boxes when submit button is clicked and run comparison functions.
 @app.callback(
     [Output('results-container', 'children'),
     Output('total_comparisons', 'children')],
@@ -116,10 +123,11 @@ def parse_contents(contents, filename):
      State('num_results', 'value')],
     prevent_initial_call=True
 )
+#function which runs the comparison functions, creates a table of results, and returns the table
 def update_output(n_clicks, dropdown_value, upload_contents, upload_filename, haplogroup, num_results):
-    if n_clicks > 0:
-        file_path = None
-        if upload_contents:
+    if n_clicks > 0: #only run if submit button has been clicked
+        file_path = None #initialize file_path variable
+        if upload_contents: #if a file has been uploaded, parse the contents and write to a temporary file
             content = parse_contents(upload_contents, upload_filename)
             file_path = f'03_tmpfiles/Filtered_User/{upload_filename}'
             with open(file_path, 'wb') as f:
@@ -128,12 +136,12 @@ def update_output(n_clicks, dropdown_value, upload_contents, upload_filename, ha
             file_path = dropdown_value
         
         try:
-            # Assuming your processing functions return a DataFrame or similar for displaying in the DataTable
+            # Run the comparison functions from UserCompareAADR.py and User_snpfilter.py scripts
             results_file_path = User_snpfilter.UserCrossref(file_path, haplogroup)
             AADR_ped_rsids, AADR_ped_meta = AADR_data
             results, total_compared = UserCompareAADR.getMatches(results_file_path, AADR_ped_rsids, AADR_ped_meta, num_results)
 
-
+            #return the results in a table, selecting only desired columns
             return html.Div([
                 dash_table.DataTable(
                     id='table',
@@ -143,7 +151,7 @@ def update_output(n_clicks, dropdown_value, upload_contents, upload_filename, ha
                     style_data_conditional=[
                         {'if': {'column_id': 'non_matching_mutations'}, 'textAlign': 'left'}
                         ]     
-                        )
+                        ) #add some text which shows the total number of comparisons and instructs you to click a row for more info
             ]),  html.Div([html.P(f'Total number of alleles compared: {total_compared}'), html.P('Click a match for additional info')])
     
         
@@ -152,12 +160,14 @@ def update_output(n_clicks, dropdown_value, upload_contents, upload_filename, ha
             error_message = f'An error occurred: {e}'
             return (html.Div(error_message), html.Div('Unable to compute total comparisons due to error'))
 
+#callback to display additional data when a row is clicked
 @app.callback(
     Output('additional-data', 'children'),
     [Input('table', 'active_cell'),
      Input('table', 'data')],
     prevent_initial_call=True
 )
+#function to get the additional data for the individual when a row is clicked
 def display_additional_data(active_cell, data):
     if active_cell:
         try:
@@ -185,5 +195,6 @@ def display_additional_data(active_cell, data):
     else:
         return html.Div()
 
+#run the app, run on port 6015 (chosen randomly, becaause default was often in use)
 if __name__ == '__main__':
     app.run_server(debug=True, port=6015)
